0b8be9d04c9bf65497afaf1ddf169372fe839454
newLana <newLana@users.noreply.github.com>
2018-09-16 23:25:22 +0300

    WeightSort Project

---
+++ CodeWars.Core/WeightSort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp
+{
+    public class WeightSort
+    {
+        public static string orderWeight(string strng)
+        {
+            return string.Join(" ", strng.Split(' ').OrderBy(p => p.Select(n => n.ToString()).Sum(j => Convert.ToInt32(j))).ThenBy(i => i));
+        }
+    }
+}
