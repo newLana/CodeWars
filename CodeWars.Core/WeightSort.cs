e341a73fb6392c936c732499bacfd02ca03c58ed
newLana <newLana@users.noreply.github.com>
2018-09-17 12:26:42 +0300

    Added main test to WeightSort

--- CodeWars.Core/WeightSort.cs
+++ CodeWars.Core/WeightSort.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Linq;
 
-namespace CSharp
+namespace CodeWars.Core
 {
     public class WeightSort
     {
         public static string orderWeight(string strng)
         {
-            return string.Join(" ", strng.Split(' ').OrderBy(p => p.Select(n => n.ToString()).Sum(j => Convert.ToInt32(j))).ThenBy(i => i));
+            return string.Join(" ", strng.Split(' ').OrderBy(p => p.Sum(j => Int32.Parse(j.ToString()))).ThenBy(i => i));
         }
     }
 }
---
+++ CodeWars.Tests/WeightSortTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using CodeWars.Core;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    public class WeightSortTest
+    {
+        [Test]
+        public void orderWeightTest()
+        {
+            string inpurString = "56 65 74 100 99 68 86 180 90";
+            var result = WeightSort.orderWeight(inpurString);
+            Assert.That(result, Is.EqualTo("100 180 90 56 65 74 68 86 99"));
+        }
+    }
+}
